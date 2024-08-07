9076b2b8c96858d130b23157131214fc17be124f
Andressa Evelyn <andressaevelyn200214@gmail.com>
2024-08-07 11:30:46 -0300

    responsividade do menu

---
+++ main.js
@@ -0,0 +1,13 @@
+// função para abrir o menu
+function openMenu() {
+    const menu = document.getElementById('menu');
+    // para dizer que o menu está aberto adicionamos a
+    // classe de estilo active
+    menu.classList.add('active');
+}
+
+// função para fechar o menu
+function closeMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.remove('active')
+}
