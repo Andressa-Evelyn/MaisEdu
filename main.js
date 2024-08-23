b771c3396542bda11a8ddc63e75ccba30432a093
Andressa Evelyn <andressaevelyn200214@gmail.com>
2024-08-23 20:28:15 -0300

    parte1 swiper

---
+++ main.js
@@ -0,0 +1,54 @@
+// função para abrir o menu
+function openMenu() {
+    const menu = document.getElementById('menu');
+    // para dizer que o menu está aberto adicionamos a
+    // classe de estilo active
+    menu.classList.add('active');
+}
+
+// função para fechar o menu
+function closeMenu() {
+    const menu = document.getElementById('menu');
+    menu.classList.remove('active')
+}
+
+function nextSlide(){
+    swiper.slideNext(3000)
+}
+
+function prevSlide(){
+    swiper.slidePrev(3000)
+}
+
+var swiper = new Swiper(".slide_container", {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    sliderPerGroup: 1,
+    loop: true,
+    centerSlide: true,
+    fade: true,
+    grabCursor: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      520: {
+        slidesPerView: 2,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      1000: {
+        slidesPerView: 4,
+      },
+    },
+  });
