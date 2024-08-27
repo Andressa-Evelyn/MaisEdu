2233799b7cf152f1f3e7e3ddb393fee78d0a9e39
Andressa Evelyn <andressaevelyn200214@gmail.com>
2024-08-27 09:55:20 -0300

    nav content

--- main.js
+++ main.js
@@ -4,12 +4,14 @@ function openMenu() {
     // para dizer que o menu está aberto adicionamos a
     // classe de estilo active
     menu.classList.add('active');
+    
 }
 
 // função para fechar o menu
 function closeMenu() {
     const menu = document.getElementById('menu');
     menu.classList.remove('active')
+
 }
 
 function nextSlide(){
